1befbf9e949d1119816b667df2d2fb5869de1d8f
Konsta Kuosmanen <konstamskuosmanen@gmail.com>
2022-10-24 15:51:48 +0200

    init commit

---
+++ patch/include.c
@@ -0,0 +1,22 @@
+/* Bar functionality */
+#include "bar_indicators.c"
+#include "bar_tagicons.c"
+#include "bar.c"
+
+#include "bar_status.c"
+#include "bar_tags.c"
+
+/* Other patches */
+#include "cool_autostart.c"
+#ifdef XINERAMA
+#include "sortscreens.c"
+#endif // XINERAMA
+#include "swallow.c"
+#include "vanitygaps.c"
+#include "warp.c"
+#include "xrdb.c"
+/* Layouts */
+#include "layout_facts.c"
+#include "layout_monocle.c"
+#include "layout_tile.c"
+
---
+++ patch/include.h
@@ -0,0 +1,21 @@
+/* Bar functionality */
+#include "bar_indicators.h"
+#include "bar_tagicons.h"
+#include "bar.h"
+
+#include "bar_status.h"
+#include "bar_tags.h"
+
+/* Other patches */
+#include "cool_autostart.h"
+#ifdef XINERAMA
+#include "sortscreens.h"
+#endif // XINERAMA
+#include "swallow.h"
+#include "vanitygaps.h"
+#include "warp.h"
+#include "xrdb.h"
+/* Layouts */
+#include "layout_monocle.h"
+#include "layout_tile.h"
+
