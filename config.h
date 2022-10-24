27549fe5bd2a716876e2fb627501bfedd3c28f5d
Konsta Kuosmanen <konstamskuosmanen@gmail.com>
2022-10-24 16:02:40 +0200

    added personal config

--- config.h
+++ config.h
@@ -1,28 +1,47 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx       = 1;   /* border pixel of windows */
-static const unsigned int snap           = 32;  /* snap pixel */
-static const int swallowfloating         = 0;   /* 1 means swallow floating windows by default */
+static const unsigned int borderpx       = 0;   /* border pixel of windows */
+static const unsigned int snap           = 8;  /* snap pixel */
+static const int swallowfloating         = 1;   /* 1 means swallow floating windows by default */
 
-static const unsigned int gappih         = 20;  /* horiz inner gap between windows */
-static const unsigned int gappiv         = 10;  /* vert inner gap between windows */
-static const unsigned int gappoh         = 10;  /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov         = 30;  /* vert outer gap between windows and screen edge */
+#ifdef desktop
+static const unsigned int gappih         = 6;  /* horiz inner gap between windows */
+static const unsigned int gappiv         = 6;  /* vert inner gap between windows */
+static const unsigned int gappoh         = 8;  /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov         = 10;  /* vert outer gap between windows and screen edge */
+#elif laptop
+static const unsigned int gappih         = 12;  /* horiz inner gap between windows */
+static const unsigned int gappiv         = 12;  /* vert inner gap between windows */
+static const unsigned int gappoh         = 12;  /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov         = 12;  /* vert outer gap between windows and screen edge */
+#endif
 
 static const int smartgaps_fact          = 1;   /* gap factor when there is only one client; 0 = no gaps, 3 = 3x outer gaps */
 static const int showbar                 = 1;   /* 0 means no bar */
-static const int topbar                  = 1;   /* 0 means bottom bar */
+static const int topbar                  = 0;   /* 0 means bottom bar */
 /* Status is to be shown on: -1 (all monitors), 0 (a specific monitor by index), 'A' (active monitor) */
-static const int statusmon               = 'A';
-
-
+static const int statusmon               = -1;
 
 /* Indicators: see patch/bar_indicators.h for options */
 static int tagindicatortype              = INDICATOR_TOP_LEFT_SQUARE;
 static int tiledindicatortype            = INDICATOR_NONE;
 static int floatindicatortype            = INDICATOR_TOP_LEFT_SQUARE;
-static const char *fonts[]               = { "monospace:size=10" };
+
+#ifdef desktop
+static const char *fonts[]          = {
+   "LiterationMono Nerd Font:pixelsize=14:antialias=true:autohint=true",
+   "JoyPixels:pixelsize=12:antialias=true:autohint=true",
+   "Latin Modern Math:pixelsize=12:antialias=true:autohint=true",
+};
+#elif laptop
+static const char *fonts[]          = {
+   "LiterationMono Nerd Font:pixelsize=20:antialias=true:autohint=true",
+   "JoyPixels:pixelsize=18:antialias=true:autohint=true",
+   "Latin Modern Math:pixelsize=18:antialias=true:autohint=true",
+};
+#endif
+
 static const char dmenufont[]            = "monospace:size=10";
 
 static char c000000[]                    = "#000000"; // placeholder value
@@ -86,7 +105,7 @@ static char *colors[][ColCount] = {
 
 
 static const char *const autostart[] = {
-    "st", NULL,
+    "zsh", "-c", "/home/$USERNAME/.config/init/init.sh", NULL,
     NULL /* terminate */
 };
 
@@ -120,7 +139,7 @@ static const char *const autostart[] = {
  */
 static char *tagicons[][NUMTAGS] =
 {
-    [DEFAULT_TAGS]        = { "1", "2", "3", "4", "5", "6", "7", "8", "9" },
+    [DEFAULT_TAGS]        = { "一", "二", "三", "四", "五", "六", "セ", "八", "九" },
     [ALTERNATIVE_TAGS]    = { "A", "B", "C", "D", "E", "F", "G", "H", "I" },
     [ALT_TAGS_DECORATION] = { "<1>", "<2>", "<3>", "<4>", "<5>", "<6>", "<7>", "<8>", "<9>" },
 };
@@ -155,8 +174,26 @@ static const Rule rules[] = {
     RULE(.wintype = WTYPE "UTILITY", .isfloating = 1)
     RULE(.wintype = WTYPE "TOOLBAR", .isfloating = 1)
     RULE(.wintype = WTYPE "SPLASH", .isfloating = 1)
-    RULE(.class = "Gimp", .tags = 1 << 4)
-    RULE(.class = "Firefox", .tags = 1 << 7)
+#ifdef desktop
+    RULE(.class = "firefox", .monitor = 1)
+
+    RULE(.title = "Sqlectron", .monitor = 1, .tags = 1 << 1)
+
+    RULE(.title = "Discord", .monitor = 0)
+
+    RULE(.class = "spotify", .monitor = 3, .tags = 1 << 1)
+    RULE(.class = "Spotify", .monitor = 3, .tags = 1 << 1)
+
+    RULE(.title = "Entertainment", .monitor = 3)
+    RULE(.title = "qBittorrent", .monitor = 3, .tags = 1 << 8)
+#endif
+    RULE(.class = "st", .isterminal = 1)
+    RULE(.class = "St", .isterminal = 1)
+
+    RULE(.class = "MultiMC 5 - Version 0.6.16-custom", .isterminal = 0)
+    RULE(.title = "Chrome", .isterminal = 0)
+    RULE(.title = "chrome", .isterminal = 0)
+    RULE(.title = "chrome", .isterminal = 0)
 };
 
 
@@ -180,23 +217,21 @@ static const BarRule barrules[] = {
 };
 
 /* layout(s) */
-static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
+static const float mfact     = 0.50; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
-static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
-static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
+static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
+static const int lockfullscreen = 0; /* 1 will force focus on the fullscreen window */
 
 
 
 static const Layout layouts[] = {
     /* symbol     arrange function */
     { "[]=",      tile },    /* first entry is default */
-    { "><>",      NULL },    /* no layout function means floating behavior */
-    { "[M]",      monocle },
 };
 
 
 /* key definitions */
-#define MODKEY Mod1Mask
+#define MODKEY Mod4Mask
 #define TAGKEYS(KEY,TAG) \
     { MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
     { MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
@@ -209,62 +244,95 @@ static const Layout layouts[] = {
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
 /* commands */
-static const char *dmenucmd[] = {
-    "dmenu_run",
-    "-fn", dmenufont,
-    "-nb", normbgcolor,
-    "-nf", normfgcolor,
-    "-sb", selbgcolor,
-    "-sf", selfgcolor,
-    NULL
-};
+static const char *dmenucmd[] = { "dmenu_run", NULL };
+static const char *sudodmenucmd[] = { "sudo", "dmenu_run", NULL };
+
 static const char *termcmd[]  = { "st", NULL };
 
+static const char *dmenuappimagecmd[] = { "dmenu-appimage", NULL };
+
+static const char *dmenuinsertcmd[] = { "dmenu-insert", NULL };
+static const char *dmenuoutputcmd[] = { "dmenu-output", NULL };
+
+static const char *dmenubluetoothcmd[] = { "st", "-e", "bluetooth", NULL };
+
+static const char *dmenurestartcmd[] = { "dmenu-restart", NULL };
+
+static const char *dmenupowercmd[] = { "dmenu-power", NULL };
+
+static const char *browsercmd[] = { "chrome", NULL };
+
+static const char *wallpapercmd[] = { "wallpaper-window", NULL };
+
+static const char *eclipsecmd[] = { "eclipse", NULL };
+
+static const char *discordcmd[] = { "discord", NULL };
+static const char *spotifycmd[] = { "spotify", NULL };
 
 
-static const Key keys[] = {
+static Key keys[] = {
     /* modifier                     key            function                argument */
-    { MODKEY,                       XK_p,          spawn,                  {.v = dmenucmd } },
-    { MODKEY|ShiftMask,             XK_Return,     spawn,                  {.v = termcmd } },
-    { MODKEY,                       XK_b,          togglebar,              {0} },
-    { MODKEY,                       XK_j,          focusstack,             {.i = +1 } },
-    { MODKEY,                       XK_k,          focusstack,             {.i = -1 } },
-    { MODKEY,                       XK_i,          incnmaster,             {.i = +1 } },
-    { MODKEY,                       XK_d,          incnmaster,             {.i = -1 } },
-    { MODKEY,                       XK_h,          setmfact,               {.f = -0.05} },
-    { MODKEY,                       XK_l,          setmfact,               {.f = +0.05} },
-    { MODKEY,                       XK_Return,     zoom,                   {0} },
-    { MODKEY|Mod4Mask,              XK_u,          incrgaps,               {.i = +1 } },
-    { MODKEY|Mod4Mask|ShiftMask,    XK_u,          incrgaps,               {.i = -1 } },
-    { MODKEY|Mod4Mask,              XK_i,          incrigaps,              {.i = +1 } },
-    { MODKEY|Mod4Mask|ShiftMask,    XK_i,          incrigaps,              {.i = -1 } },
-    { MODKEY|Mod4Mask,              XK_o,          incrogaps,              {.i = +1 } },
-    { MODKEY|Mod4Mask|ShiftMask,    XK_o,          incrogaps,              {.i = -1 } },
-    { MODKEY|Mod4Mask,              XK_6,          incrihgaps,             {.i = +1 } },
-    { MODKEY|Mod4Mask|ShiftMask,    XK_6,          incrihgaps,             {.i = -1 } },
-    { MODKEY|Mod4Mask,              XK_7,          incrivgaps,             {.i = +1 } },
-    { MODKEY|Mod4Mask|ShiftMask,    XK_7,          incrivgaps,             {.i = -1 } },
-    { MODKEY|Mod4Mask,              XK_8,          incrohgaps,             {.i = +1 } },
-    { MODKEY|Mod4Mask|ShiftMask,    XK_8,          incrohgaps,             {.i = -1 } },
-    { MODKEY|Mod4Mask,              XK_9,          incrovgaps,             {.i = +1 } },
-    { MODKEY|Mod4Mask|ShiftMask,    XK_9,          incrovgaps,             {.i = -1 } },
-    { MODKEY|Mod4Mask,              XK_0,          togglegaps,             {0} },
-    { MODKEY|Mod4Mask|ShiftMask,    XK_0,          defaultgaps,            {0} },
-    { MODKEY,                       XK_Tab,        view,                   {0} },
-    { MODKEY|ShiftMask,             XK_c,          killclient,             {0} },
-    { MODKEY|ShiftMask,             XK_q,          quit,                   {0} },
-    { MODKEY|ShiftMask,             XK_F5,         xrdb,                   {.v = NULL } },
-    { MODKEY,                       XK_t,          setlayout,              {.v = &layouts[0]} },
-    { MODKEY,                       XK_f,          setlayout,              {.v = &layouts[1]} },
-    { MODKEY,                       XK_m,          setlayout,              {.v = &layouts[2]} },
-    { MODKEY,                       XK_space,      setlayout,              {0} },
-    { MODKEY|ShiftMask,             XK_space,      togglefloating,         {0} },
-    { MODKEY,                       XK_0,          view,                   {.ui = ~0 } },
-    { MODKEY|ShiftMask,             XK_0,          tag,                    {.ui = ~0 } },
-    { MODKEY,                       XK_comma,      focusmon,               {.i = -1 } },
-    { MODKEY,                       XK_period,     focusmon,               {.i = +1 } },
-    { MODKEY|ShiftMask,             XK_comma,      tagmon,                 {.i = -1 } },
-    { MODKEY|ShiftMask,             XK_period,     tagmon,                 {.i = +1 } },
+    { MODKEY,                       XK_space,  spawn,          {.v = dmenucmd } },
+    { MODKEY|ShiftMask,             XK_space,  spawn,          {.v = sudodmenucmd } },
+
+    { MODKEY,                       XK_a,      spawn,          {.v = dmenuappimagecmd } },
+
+    { MODKEY,                       XK_i,      spawn,          {.v = dmenuinsertcmd } },
+    { MODKEY,                       XK_o,      spawn,          {.v = dmenuoutputcmd } },
+
+    { MODKEY,                       XK_b,      spawn,          {.v = dmenubluetoothcmd } },
+
+    { MODKEY,                       XK_r,      spawn,          {.v = dmenurestartcmd } },
+
+    { MODKEY,                       XK_F4,     spawn,          {.v = dmenupowercmd } },
+
+    { MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
+
+    // Volume keys
+    { MODKEY|ShiftMask,             XK_comma,  spawn,          SHCMD("volume -d 5") },
+    { MODKEY|ShiftMask,             XK_period, spawn,          SHCMD("volume -i 5") },
+
+    { 0,              XF86XK_AudioRaiseVolume, spawn,          SHCMD("volume -i 5") },
+    { 0,              XF86XK_AudioLowerVolume, spawn,          SHCMD("volume -d 5") },
+    { 0,              XF86XK_AudioMute,        spawn,          SHCMD("volume -m") },
+
+    { 0,              XF86XK_AudioPlay,        spawn,          SHCMD("mpris-ctl pp") },
+    { 0,              XF86XK_AudioPause,       spawn,          SHCMD("mpris-ctl pp") },
+    { 0,              XF86XK_AudioStop,        spawn,          SHCMD("mpris-ctl pause") },
+    { 0,              XF86XK_AudioNext,        spawn,          SHCMD("mpris-ctl --player active next") },
+    { 0,              XF86XK_AudioPrev,        spawn,          SHCMD("mpris-ctl --player active prev") },
+
+    { 0,              XF86XK_MonBrightnessUp,  spawn,          SHCMD("brightness -i 5") },
+    { 0,              XF86XK_MonBrightnessDown,spawn,          SHCMD("brightness -d 5") },
+
+    { MODKEY,                       XK_c,      spawn,          {.v = browsercmd } },
+    { MODKEY,                       XK_w,      spawn,          {.v = wallpapercmd } },
+    { MODKEY,                       XK_e,      spawn,          {.v = eclipsecmd } },
+    { MODKEY,                       XK_s,      spawn,          {.v = spotifycmd } },
+    { MODKEY,                       XK_d,      spawn,          {.v = discordcmd } },
+
+    { MODKEY,                       XK_f,      togglebar,      {0} },
+
+    { MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
+    { MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
+
+    { MODKEY,                       XK_p,      zoom,           {0} },
+    { MODKEY,                       XK_Tab,    view,           {0} },
+
+    { MODKEY|ShiftMask,             XK_c,      quit,           {0} },
+    { MODKEY,                       XK_q,      killclient,     {0} },
+
+    { MODKEY,                       XK_h,      focusmon,       {.i = -1 } },
+    { MODKEY,                       XK_l,      focusmon,       {.i = +1 } },
+
+    { MODKEY|ShiftMask,             XK_h,      focustagmon,    {.i = -1 } },
+    { MODKEY|ShiftMask,             XK_l,      focustagmon,    {.i = +1 } },
+
+    // Music keys
+    { MODKEY,                       XK_comma,  spawn,          SHCMD("mpris-ctl --player active prev") },
+    { MODKEY,                       XK_period, spawn,          SHCMD("mpris-ctl --player active pp") },
+    { MODKEY,                       XK_slash,  spawn,          SHCMD("mpris-ctl --player active next") },
+
     TAGKEYS(                        XK_1,                                  0)
     TAGKEYS(                        XK_2,                                  1)
     TAGKEYS(                        XK_3,                                  2)
--- config.mk
+++ config.mk
@@ -57,12 +57,13 @@ XRENDER = -lXrender
 
 # includes and libs
 INCS = -I${X11INC} -I${FREETYPEINC} ${YAJLINC} ${PANGOINC} ${BDINC}
-LIBS = -L${X11LIB} -lX11 ${XINERAMALIBS} ${FREETYPELIBS}  ${XRENDER} ${MPDCLIENT} ${XEXTLIB} ${XCBLIBS} ${KVMLIB} ${PANGOLIB} ${YAJLLIBS} ${IMLIB2LIBS} $(BDLIBS)
+LIBS = -L${X11LIB} -lX11 ${XINERAMALIBS} ${FREETYPELIBS}  ${XRENDER} ${MPDCLIENT} ${XEXTLIB} ${XCBLIBS} ${KVMLIB} ${PANGOLIB} ${YAJLLIBS} ${IMLIB2LIBS} $(BDLIBS) -lX11-xcb -lxcb -lxcb-res
 
 # flags
 CPPFLAGS = -D_DEFAULT_SOURCE -D_BSD_SOURCE -D_POSIX_C_SOURCE=200809L -DVERSION=\"${VERSION}\" ${XINERAMAFLAGS}
 #CFLAGS   = -g -std=c99 -pedantic -Wall -O0 ${INCS} ${CPPFLAGS}
-CFLAGS   = -std=c99 -pedantic -Wall -Wno-unused-function -Wno-deprecated-declarations -Os ${INCS} ${CPPFLAGS}
+HOST := $(shell cat /proc/sys/kernel/hostname)
+CFLAGS	 = -std=c99 -D$(HOST) -pedantic -Wall -Wno-unused-function -Wno-deprecated-declarations -Os ${INCS} ${CPPFLAGS}
 LDFLAGS  = ${LIBS}
 
 # Solaris
